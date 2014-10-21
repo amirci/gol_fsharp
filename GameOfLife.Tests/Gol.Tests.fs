b2563bf630a27891e4777a16e6c1422d69297967
Amir Barylko <amir@barylko.com>
2014-10-21 12:46:37 -0600

    First test failing

---
+++ GameOfLife.Tests/Gol.Tests.fs
@@ -0,0 +1,22 @@
+﻿module GameOfLife.Tests
+
+open NUnit.Framework
+open FsUnit
+
+open GameOfLife
+
+
+[<Test>]
+let ``A blinker turns from horizontal to vertical`` () =
+    let blinker = CellPattern [x ; x ; x ;
+                               x ; o ; x ;
+                               x ; o ; x ;
+                               x ; o ; x ]
+
+
+    let invertedBlinker = CellPattern [x ; x ; x ;
+                                       o ; o ; o ;]
+
+    GameOfLife.Evolve blinker |> should equal invertedBlinker
+    GameOfLife.Evolve invertedBlinker |> should equal blinker
+        
---
+++ GameOfLife/Gol.fs
@@ -0,0 +1,16 @@
+﻿module GameOfLife
+
+
+type Cell =
+    | Alive
+    | Dead
+
+let x = Alive
+let o = Dead
+
+type CellPattern (cells: Cell List) = 
+
+    member this.x = "F#"
+
+let Evolve (pattern: CellPattern) =
+    pattern

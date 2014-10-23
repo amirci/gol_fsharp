8d7a9149e50c7cb1a678ed95cb86e222882f8d73
Amir Barylko <amir@barylko.com>
2014-10-23 11:52:23 -0600

    Added parsing of board and implemented equality

--- GameOfLife.Tests/Gol.Tests.fs
+++ GameOfLife.Tests/Gol.Tests.fs
@@ -5,17 +5,29 @@ open FsUnit
 
 open GameOfLife
 
+[<Test>]
+let ``A block never changes`` () =
+    let block = CellPattern(3, [o ; o ;
+                                o ; o ;])
+
+    let sameBlock = CellPattern(3, [o ; o ;
+                                    o ; o ;])
+
+
+    GameOfLife.Evolve block |> should equal sameBlock
+
 
 [<Test>]
 let ``A blinker turns from horizontal to vertical`` () =
-    let blinker = CellPattern [x ; x ; x ;
-                               x ; o ; x ;
-                               x ; o ; x ;
-                               x ; o ; x ]
+    let blinker = CellPattern(3, [x ; x ; x ;
+                                  x ; o ; x ;
+                                  x ; o ; x ;
+                                  x ; o ; x ])
 
 
-    let invertedBlinker = CellPattern [x ; x ; x ;
-                                       o ; o ; o ;]
+    let invertedBlinker = CellPattern(3, [x ; x ; x ;
+                                          x ; x ; x ;
+                                          o ; o ; o ;])
 
     GameOfLife.Evolve blinker |> should equal invertedBlinker
     GameOfLife.Evolve invertedBlinker |> should equal blinker
--- GameOfLife/Gol.fs
+++ GameOfLife/Gol.fs
@@ -1,6 +1,5 @@
 ﻿module GameOfLife
 
-
 type Cell =
     | Alive
     | Dead
@@ -8,9 +7,43 @@ type Cell =
 let x = Alive
 let o = Dead
 
-type CellPattern (cells: Cell List) = 
+type Position = int * int
 
-    member this.x = "F#"
+type CellPattern (size: int, startCells: Cell List) = 
+
+    let rec chop segmentSize source = 
+        seq { 
+                if Seq.isEmpty source then () else
+                let segment = source |> Seq.truncate segmentSize
+                let rest = source |> Seq.skip (Seq.length segment)
+                yield segment
+                yield! chop segmentSize rest 
+        }
+    
+    let parseCells board =
+        let cell2Pos row col state =
+            match state with 
+            | Alive -> Some(row, col)
+            | _ -> None
+
+        let row2Pos row cells = cells |> Seq.mapi (cell2Pos row)
+
+        let map2Cells cells = cells |> Seq.mapi row2Pos
+
+        chop 3 board
+        |> map2Cells
+        |> Seq.concat
+        |> Seq.choose id
+
+    member this.Parsed = parseCells startCells
+
+    override this.Equals obj =
+        match obj with
+        | :? CellPattern as other -> this.Parsed = other.Parsed
+        | _ -> false
+
+    override this.GetHashCode () =
+        startCells.GetHashCode()
 
 let Evolve (pattern: CellPattern) =
     pattern
